02509a00d88d7724f5ffa3295293fe999f479e34
Copley <ccopley1@una.edu>
2026-01-13 13:19:07 -0600

    asdsafd

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//app.js
+//es5 syntax => import express from 'express'
+//es6 syntax => import { name } from 'module-name'
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
+//starts the server 
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
